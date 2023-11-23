2a5b7a53e42599db25d2329a73ac30b68a87c2e3
Marwa <marwahmed@gmail.com>
2023-11-23 15:06:57 -0800

    Step 1: Initial Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Secret Bookshelf </h1>
+        <p>Welcome to my secret book shelf!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
